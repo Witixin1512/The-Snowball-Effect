4bea8c9f434986da6c4b7d81a94388a0ef31b679
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-08-09 21:25:18 +0200

    Make resource locations constant because I forgot

--- src/main/java/net/witixin/snowballeffect/client/igloof/ModelIgloof.java
+++ src/main/java/net/witixin/snowballeffect/client/igloof/ModelIgloof.java
@@ -6,21 +6,27 @@ import net.witixin.snowballeffect.entity.EntityIgloof;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class ModelIgloof extends AnimatedGeoModel<EntityIgloof> {
+    
+    private static final ResourceLocation MODEL_LOCATION = Reference.rl("geo/igloofy.geo.json");
+    private static final ResourceLocation ANIM_LOCATION = Reference.rl("animations/igloof.animation.json");
+    private static final ResourceLocation ICEY_TEXTURE_LOCATION = Reference.rl("textures/ice_igloof.png"));
+    private static final ResourceLocation REGULAR_TEXTURE_LOCATION = Reference.rl("textures/igloof.png");
+
     @Override
     public ResourceLocation getModelLocation(EntityIgloof object)
     {
-        return Reference.rl("geo/igloofy.geo.json");
+        return MODEL_LOCATION;
     }
 
     @Override
     public ResourceLocation getTextureLocation(EntityIgloof object)
     {
-        return (!object.isIcey() ? Reference.rl("textures/igloof.png") : Reference.rl("textures/ice_igloof.png"));
+        return (object.isIcey() ? ICEY_TEXTURE_LOCATION : REGULAR_TEXTURE_LOCATION);
     }
 
     @Override
     public ResourceLocation getAnimationFileLocation(EntityIgloof object)
     {
-        return Reference.rl("animations/igloof.animation.json");
+        return ANIM_LOCATION;
     }
 }
